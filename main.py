2b71275254a6f1707932f66f6050216e4affc413
Wojciech Radzimowski <wojciech.radzimowski.99@gmail.com>
2024-02-18 16:15:47 +0100

    Added initial list calculation and printing

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 DISCORD_GUILD = os.getenv("DISCORD_GUILD")
 CHANNEL_ID = int(os.getenv("DISCORD_CHANNEL_ID"))
 THUMBS_DOWN = ['👎', '👎🏼']
+INITIAL_ANNOUNCEMENT = """**RUNDA I**
+Głosujemy do 18:00 jutro na album który ma odpaść (emotką 👎 ) i nie można zmieniać głosów"""
 ANNOUNCEMENT = """Odpada **%album%** %reactions% głosami %draw_msg%
 Głosujemy do 18:00 jutro na album który ma odpaść (emotką 👎 ) i nie można zmieniać głosów
 **RUNDA %round%**"""
@@ -92,6 +94,49 @@ class Client(discord.Client):
 
     async def on_ready(self) -> None:
         await self.time_loop()
+
+    async def on_message(self, message: discord.Message) -> None:
+        if message.content.startswith('!list'):
+            file_name = message.content.split(' ')[1]
+            await self.print_list(file_name)
+
+    async def print_list(self, file_name) -> None:
+        channel = self.get_channel(CHANNEL_ID)
+        user = ""
+        albums_points = {}
+        albums_individual_votes = {}
+        for element in open(file_name, "r", encoding="utf-8").readlines():
+            if "-" not in element:
+                user = element.strip()
+            else:
+                album = element.split(";")[-1].strip()
+                points = int(element.split(";")[0])
+                if album in albums_points:
+                    albums_points[album] += points
+                else:
+                    albums_points[album] = points
+                    albums_individual_votes[album] = {}
+                albums_individual_votes[album][user] = points
+        template = "%place%. %points% pkt - **%album%** _(%users%)_"
+        albums_list = sorted(albums_points.items(), key=lambda item: item[1])
+        for index, album in enumerate(albums_list):
+            place = len(albums_points) - index
+            if place > INITIAL_LEN:
+                entry = template.replace("%place%", str(place)).replace("%points%",
+                                                                                             str(album[1])).replace(
+                    "%album%", album[0])
+                users_list = []
+                for user, points in sorted(albums_individual_votes[album[0]].items(), key=lambda item: item[1],
+                                           reverse=True):
+                    users_list.append(user + " - " + str(points))
+                entry = entry.replace("%users%", ", ".join(users_list))
+                await channel.send(entry)
+            else:
+                await channel.send(INITIAL_ANNOUNCEMENT)
+                albums_list_final = sorted([album[0] for album in albums_list[-INITIAL_LEN:]])
+                for album_final in albums_list_final:
+                    await channel.send(album_final)
+                break
 
 
 client = Client(DISCORD_TOKEN, DISCORD_GUILD)
